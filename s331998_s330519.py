f9961b19f95f175f76808642540220a2b270ccbe
Andrea Delli <andrea.delli2001@gmail.com>
2025-04-24 00:30:55 +0200

    complete crossover, ensure max_depth, additional operators

--- s331998_s330519.py
+++ s331998_s330519.py
@@ -19,16 +19,19 @@ def f2(x: np.ndarray) -> np.ndarray:
     #return (((x[0] / (x[1] / (x[2] + x[2]))) - ((np.sin(x[1]) + np.cos(x[2])) + np.tan((x[2] - x[2])))) + np.log(np.exp((np.sin(x[1]) / 4.62))))
     #return np.multiply(np.multiply(73.05, np.multiply(np.abs(23.51), 7.13)), np.multiply(x[0], 93.4))
     #return 1_000_000 * np.add(np.add(np.divide(np.add(x[2], np.add(np.add(x[1], x[1]), np.add(x[2], np.exp(x[0])))), np.exp(np.abs(x[0]))), x[0]), np.sin(np.multiply(np.sin(np.cos(np.cos(-4.82))), x[0])))
-    return np.power(np.exp(np.log(np.subtract(np.subtract(26.87, x[0]), np.subtract(x[1], np.sin(x[1]))))), x[0])
+    # return np.power(np.exp(np.log(np.subtract(np.subtract(26.87, x[0]), np.subtract(x[1], np.sin(x[1]))))), x[0])
+    return 56465.10655809015 + 5441856.124013525 * (np.sin(np.sin((np.tan(np.abs(np.sin(np.sin(np.sin((np.cos(np.sin(np.sin(0.422))) + (0.414 * np.sin((np.cos((x[0] - (np.exp(x[2]) ** np.log(np.sin(0.085))))) - x[0]))))))))) * (x[0] * np.cos(np.exp(((x[2] * (0.596 ** x[0])) * (np.tan(-0.018) * np.sin(x[0]))))))))))
 
 def f3(x: np.ndarray) -> np.ndarray:
     #return (np.cos(2.24) + ((x[1] * x[1]) * (x[1] * np.cos(2.24))))
     #return np.multiply(np.divide(np.abs(x[1]), np.sin(34.82)), x[1])
-    return np.add(np.add(np.abs(np.multiply(np.abs(x[0]), 2.19)), np.subtract(np.subtract(np.exp(np.subtract(np.abs(np.sin(np.exp(x[1]))), x[1])), np.exp(x[1])), -8.93)), np.multiply(np.abs(x[0]), 2.19))
+    # return np.add(np.add(np.abs(np.multiply(np.abs(x[0]), 2.19)), np.subtract(np.subtract(np.exp(np.subtract(np.abs(np.sin(np.exp(x[1]))), x[1])), np.exp(x[1])), -8.93)), np.multiply(np.abs(x[0]), 2.19))
+    return 21.20757039569563 + 50.61161179521197 * (((np.tan(np.sin(x[1])) - (np.cos(np.cos(np.exp(np.cos(np.abs(x[1]))))) * x[1])) * np.tan(0.352)))
+
 
 def f4(x: np.ndarray) -> np.ndarray: 
     #return (np.cos(x[1]) + (np.exp(np.cos(x[1])) * np.exp(np.cos(x[1]))))
-    return np.exp(-44.46)
+    return 1.9997861267141543 + 4.649946409222459 * (np.tan(np.cos((np.cos(np.sin(np.abs(np.log((np.sin(np.abs(np.sin((-0.056 / x[1])))) ** 0.73))))) * x[1]))))
 
 def f5(x: np.ndarray) -> np.ndarray: 
     #return np.sin((np.sin(np.tan(x[0])) * np.log(((x[1] * x[0]) / (x[0] + x[0])))))
