1f7ccae0505680efec80ba65f0734f8bb0b0c0b4
giorgiamodi <giorgia.modi@stud.unifi.it>
2025-06-05 17:50:58 +0200

    clean output

--- s331998_s330519.py
+++ s331998_s330519.py
@@ -7,17 +7,11 @@ import numpy as np
 # All numpy's mathematical functions can be used in formulas
 # see: https://numpy.org/doc/stable/reference/routines.math.html
 
-
-# Notez bien: No need to include f0 -- it's just an example!
-def f0(x: np.ndarray) -> np.ndarray:
-    return x[0] + np.sin(x[1]) / 5
-
 def f1(x: np.ndarray) -> np.ndarray:
     return np.sin(x[0])
 
 def f2(x: np.ndarray) -> np.ndarray:
     return (((((17.976 + (13.556 + 98.991)) + ((34.785 + np.cos(x[2])) - (x[0] * (x[0] + x[0])))) * (((7.68 + np.cos(x[2])) + (x[2] + 98.991)) + (((x[0] * x[2]) - (x[0] * x[1])) - (x[0] * (x[1] + x[0]))))) * (((((x[1] * x[2]) - np.exp(x[2])) + (98.991 + 84.959)) + ((90.002 / x[0]) * (x[1] + x[2]))) + ((((11.47 * x[2]) + (x[1] + x[0])) + ((x[1] + x[0]) + (x[1] + x[0]))) * (((x[0] + x[0]) / (x[0] * 5)) - (x[1] + x[0]))))) * ((np.tanh(((x[0] - (x[0] - x[1])) + 6)) * x[0]) / (((np.exp(np.sin(x[2])) + np.abs((x[1] + x[0]))) + (((x[0] - x[1]) + np.exp(x[1])) - np.tanh((x[0] / x[2])))) ** ((((x[2] + x[2]) + (x[1] + x[0])) * x[2]) / (((x[2] - 34.785) + (x[2] + 350)) + (np.abs(x[0]) * (x[0] * x[1])))))))
-
 
 
 def f3(x: np.ndarray) -> np.ndarray:
@@ -43,9 +37,10 @@ def f7(x: np.ndarray) -> np.ndarray:
 def f8(x: np.ndarray) -> np.ndarray: 
     return ((np.exp(x[3]) - (((36.419 - (((-48.79 + -40.34) - (((33.294 + (36.419 - x[5])) / np.exp(np.cos(x[5]))) ** np.cos(x[4]))) * (np.cos(np.sin(np.exp((x[4] + -40.34)))) - (np.sin((x[4] + (-11.033 + x[4]))) - (np.abs(np.cos((x[5] - 27.598))) + np.sin(x[5])))))) + ((((x[4] + np.sin(x[3])) - ((np.sin((np.exp(-1.663) - (x[4] + x[4]))) - ((x[4] + x[4]) + x[4])) * (-1.663 - (x[4] + (x[4] + x[4]))))) + (np.exp(np.tan(np.sin(x[3]))) - ((x[3] - 41.92) * (np.cos(x[4]) - x[3])))) - ((np.sin((np.tan(np.cos(-1.663)) - (x[4] + x[4]))) - (x[4] + (np.cos(np.log(26.12)) + (x[4] + x[4])))) * ((np.sin((np.tan(np.cos(-1.663)) - (x[4] + x[4]))) - (x[4] + np.sin(43.067))) - x[4])))) - ((-48.79 * np.sin(x[5])) + x[5]))) + (np.abs(-21.878) * ((((np.abs(np.abs(33.294)) - ((np.exp(-1.663) - (x[4] + x[4])) * (np.cos(-1.663) - x[4]))) + -22.214) * np.exp(np.exp((np.exp((np.abs(-16.989) - ((np.sin(x[4]) - (x[4] + x[4])) * ((41.697 ** -11.033) - (x[4] + x[4]))))) * -26.183)))) + (np.exp(np.abs(x[5])) * x[5]))))
 
+
 # Compute the MSE of the functions
 if __name__ == "__main__":
-    for i in range(9):
+    for i in range(1, 9):
         # Load the function
         f = eval(f"f{i}")
 
@@ -56,4 +51,4 @@ if __name__ == "__main__":
 
         # Compute the MSE
         mse = np.mean((f(x) - y) ** 2)
-        print(f"f{i} MSE: {mse:.3e} " + ('✅' if mse < 1e-3 else '❌'))
+        print(f"f{i} MSE: {mse:.5e} ")
